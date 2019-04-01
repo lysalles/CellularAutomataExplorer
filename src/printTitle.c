e0268fd2a57f85112b3179799957a1b52502adba
Ly Sandro Amorim de Campos Salles <46694835+Ly54ndr0@users.noreply.github.com>
2019-04-01 18:21:29 -0300

    printTitle is now 1 line more compact

--- src/printTitle.c
+++ src/printTitle.c
@@ -1,26 +1,20 @@
 #include "printTitle.h"
 #include <stdio.h>
 
-#define PUBLIC // Empty
-#define PRIVATE static
-
-PRIVATE void printSeparator(int n)
+static void printSeparator(int n)
 {
 	for (int i = 0; i < n; ++i)
 		putchar('~');
 	putchar('\n');
 }
 
-PUBLIC void printTitle(void)
+void printTitle(void)
 {
 	puts(TITLE);
 	puts("Written by " AUTHOR);
 	puts(INSTITUTION);
-	puts(DATE);
-	
-	printSeparator(33);
-	puts("Compiled on " __TIME__ " " __DATE__);
-	printSeparator(33);
+	puts("Written in " DATE ". " "Compiled on " __TIME__ " " __DATE__);
+	//puts("Compiled on " __TIME__ " " __DATE__);
 
 	putchar('\n');
 }
