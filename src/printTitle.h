65eaf006403db3e1ea02e493d376a0c8066d7ae4
Ly Sandro Amorim de Campos Salles <46694835+Ly54ndr0@users.noreply.github.com>
2019-04-01 13:56:02 -0300

    Added function description in printTitle.h

--- src/printTitle.h
+++ src/printTitle.h
@@ -6,6 +6,10 @@
 #define INSTITUTION "Universidade Federal do Parana (UFPR)"
 #define DATE "2019"
 
+/*
+printTitle:
+Prints the title, author, intitution, date of production and date of compilation of this program.
+*/
 void printTitle(void);
 
 #endif
